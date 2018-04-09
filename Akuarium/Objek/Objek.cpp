d8c420179ff892b25162f5e937b0c868a64add84
Albert Sahala <32014975+Kaapo29@users.noreply.github.com>
2018-04-09 21:12:40 +0700

    Add files via upload

--- Akuarium/Objek/Objek.cpp
+++ Akuarium/Objek/Objek.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
 #include "Objek.h"
-#include "../Akuarium.h"
+#include "oop.hpp"
+#include <bits/stdc++.h>
 
-Objek::Objek(int _x, int _y){
-	x = _x;
-	y = _y;
+using namespace std;
+
+const double speedObjek = 40;
+
+Objek :: Objek(int _x,int _y) : Akuarium(_x,_y){
 }
-	
-/* Method untuk pergerakan objek */
-void Objek::Move(){
-	
+
+void Objek :: Move() {
+	double prevtimne = time_since_start();
+	double cyObjek = SCREEN_HEIGHT2;
+	while(cyObjek > 0){
+		double now = time_since_start();
+        double sec_since_last = now - prevtime;
+        prevtime = now
+		cy -= speedObjek * sec_since_last;
+	}
+
 }
